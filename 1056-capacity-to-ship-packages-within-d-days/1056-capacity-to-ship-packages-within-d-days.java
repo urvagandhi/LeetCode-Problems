b3055aaebf9f0079fcb8d6343eb7a9c4f3422d07
Urva Gandhi <161921384+UrvaGandhi24@users.noreply.github.com>
2025-05-25 14:32:20 +0530

    Time: 11 ms (75.74%), Space: 47.6 MB (28.04%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int start = 0;
+        int end = 0;
+
+        for (int i : weights) {
+            start = Math.max(start, i);
+            end += i;
+        }
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int totaldays = require(weights, mid);
+
+            if (totaldays >= days) start = mid + 1;
+            else end = mid - 1;
+                
+        }
+        return start;
+    }
+    public int require(int[] weights, int mid){
+        int sum = 0;
+        int days = 0;
+        for(int weight : weights){
+            if(sum + weight > mid){
+                days++;
+                sum = weight;
+            } else sum+= weight;
+        }
+        return days;
+    }
+}

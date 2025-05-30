c0b837080969d8a0424940495087b7a7aa928d16
Urva Gandhi <161921384+UrvaGandhi24@users.noreply.github.com>
2025-05-30 13:10:34 +0530

    Time: 0 ms (100%), Space: 44.7 MB (40.58%) - LeetHub

--- 1651-shuffle-string/1651-shuffle-string.java
+++ 1651-shuffle-string/1651-shuffle-string.java
@@ -1,27 +1,11 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
-        char[] chars = s.toCharArray();
-        int i = 0;
-        while (i < indices.length) {
-            if (i != indices[i]) {
-                swap(chars, i, indices[i]);
-                swap(indices, i, indices[i]);
-            } else {
-                i++;
-            }
+        char[] result = new char[s.length()];
+        
+        for (int i = 0; i < s.length(); i++) {
+            result[indices[i]] = s.charAt(i);
         }
-        return new String(chars);
-    }
-
-    static void swap(char[] arr, int first, int second) {
-        char temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
-    }
-
-    static void swap(int[] arr, int first, int second) {
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
+        
+        return new String(result);
     }
 }

25bbdf6dd45ed06dcc990fcc22355a3afb881b5c
Urva Gandhi <161921384+UrvaGandhi24@users.noreply.github.com>
2025-05-30 14:02:10 +0530

    Time: 0 ms (100%), Space: 41.5 MB (59.76%) - LeetHub

--- 1970-sorting-the-sentence/1970-sorting-the-sentence.java
+++ 1970-sorting-the-sentence/1970-sorting-the-sentence.java
@@ -1,14 +1,28 @@
 class Solution {
     public String sortSentence(String s) {
         String[] words = s.split(" ");
-        String[] result = new String[words.length];
-        
-        for (String word : words) {
-            int len = word.length();
-            int pos = word.charAt(len - 1) - '1';
-            result[pos] = word.substring(0, len - 1);
+        int i = 0;
+
+        while (i < words.length) {
+            int index = words[i].charAt(words[i].length() - 1) - '1';
+
+            if (i != index) {
+                swap(words, i, index);
+            } else {
+                i++;
+            }
         }
 
-        return String.join(" ", result);
+        for (int j = 0; j < words.length; j++) {
+            words[j] = words[j].substring(0, words[j].length() - 1);
+        }
+
+        return String.join(" ", words);
+    }
+
+    static void swap(String[] arr, int first, int second) {
+        String temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
 }

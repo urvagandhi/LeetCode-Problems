66a038dca0d9933e3ed76f287862f06ac20e1c7b
Urva Gandhi <161921384+UrvaGandhi24@users.noreply.github.com>
2025-05-30 16:39:51 +0530

    Time: 2 ms (46.62%), Space: 40.6 MB (28.82%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,6 +1,9 @@
 class Solution {
     public int fib(int n) {
-        if(n <= 1) return n;
-        return fib(n-1) + fib(n-2);
+        if (n <= 1)
+            return n;
+        else if (n < 5)
+            return n - 1;
+        return fib(n - 1) + fib(n - 2);
     }
 }

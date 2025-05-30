b52275b560773e99a6ea9552b0997cc5a1a53ee9
Urva Gandhi <161921384+UrvaGandhi24@users.noreply.github.com>
2025-05-30 14:11:13 +0530

    Time: 0 ms (100%), Space: 41.7 MB (43.63%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.java
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -1,5 +1,23 @@
 class Solution {
     public String toLowerCase(String s) {
+
+        // String s1  = "";
+        // for(int i =0;i<s.length();i++)
+        // {
+        //     char c = s.charAt(i);
+        //     if(c >= 'A' && c <= 'Z')
+        //     {
+               
+        //        char ch = (char) (c + 32);
+        //        s1+=ch;
+        //     }
+        //     else
+        //     {
+        //         s1+=c;
+        //     }
+        // }
+
+        // return s1;
         return s.toLowerCase();
     }
 }

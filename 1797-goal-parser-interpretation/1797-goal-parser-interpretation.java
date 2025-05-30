422e5889b6e60315e3b87410491b3db85fbc7bee
Urva Gandhi <161921384+UrvaGandhi24@users.noreply.github.com>
2025-05-30 13:23:00 +0530

    Time: 0 ms (100%), Space: 41.7 MB (43.65%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.java
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.java
@@ -1,5 +1,19 @@
 class Solution {
     public String interpret(String command) {
-        return command.replace("()", "o").replace("(al)", "al");
+        StringBuilder output = new StringBuilder();
+        char[] chars = command.toCharArray();
+        for(int i = 0; i < command.length();){
+            if(chars[i] == 'G'){
+                output.append("G");
+                i++;
+            }else if(chars[i] == '(' && chars[i+1] == ')'){
+                output.append("o");
+                i+=2;
+            }else if(chars[i] == '(' && chars[i+1] == 'a'){
+                output.append("al");
+                i+=4;
+            }
+        }
+        return output.toString();
     }
 }

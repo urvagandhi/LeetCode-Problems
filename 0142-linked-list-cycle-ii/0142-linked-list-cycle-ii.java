6d0a12f2e22a8ca1163d0679311373a4f7f1e6c3
Urva Gandhi <161921384+urvagandhi@users.noreply.github.com>
2025-08-24 00:05:07 +0530

    Time: 0 ms (100%), Space: 44.6 MB (68.51%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -10,42 +10,22 @@
  * }
  */
 public class Solution {
-    private int lengthCycle(ListNode slow) {
-        ListNode curr = slow;
-        int length = 0;
-        do {
-            curr = curr.next;
-            length++;
-        } while (curr != slow);
-        return length;
-    }
     public ListNode detectCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
-        int length = 0;
 
-        while (fast != null && fast.next != null) {
+        while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
-            if (slow == fast) {
-                length = lengthCycle(slow);
-                break;
+            if(slow == fast){
+                slow=head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
             }
         }
-
-        if (length == 0) return null;
-        
-        ListNode f = head;
-        ListNode s = head;
-
-        while (length > 0) {
-            s = s.next;
-            length--;
-        }
-        while (f != s) {
-            f  = f.next;
-            s = s.next;
-        }
-        return s;
+        return null;
     }
 }

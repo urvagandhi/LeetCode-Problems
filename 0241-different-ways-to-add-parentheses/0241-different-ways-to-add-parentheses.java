3ba9f9305358da2da58f89f5a1e1b4a0902a40f0
Urva Gandhi <161921384+urvagandhi@users.noreply.github.com>
2025-08-26 18:23:25 +0530

    Time: 1 ms (99.94%), Space: 41.9 MB (83.24%) - LeetHub

---
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    HashMap<String, List<Integer>> m = new HashMap<>();
+
+    public List<Integer> solveRec(String s) {
+        if (m.containsKey(s))
+            return m.get(s);
+
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '+' || c == '-' || c == '*') {
+                List<Integer> left = solveRec(s.substring(0, i));
+                List<Integer> right = solveRec(s.substring(i + 1));
+
+                for (int l : left) {
+                    for (int r : right) {
+                        if (c == '+')
+                            ans.add(l + r);
+                        else if (c == '-')
+                            ans.add(l - r);
+                        else
+                            ans.add(l * r);
+                    }
+                }
+            }
+        }
+        if (ans.isEmpty())
+            ans.add(Integer.parseInt(s));
+        m.put(s, ans);
+        return ans;
+    }
+
+    public List<Integer> diffWaysToCompute(String expression) {
+        return solveRec(expression);
+    }
+}

a4dd07143fba42c4e03f1153f3d67120d0263e50
Urva Gandhi <161921384+urvagandhi@users.noreply.github.com>
2026-02-07 17:32:40 +0530

    1046-MAX-CONSECUTIVE-ONES-III Stats: Time: 4 ms (48.26%), Space: 51.7 MB (97.81%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -7,7 +7,7 @@ class Solution {
             if(nums[r] == 0){
                 zeros++;
             }
-            while(zeros > k){
+            if(zeros > k){
                 if(nums[l] == 0) zeros--;
                 l++;
             }

fdebe9ea5b013928645d06e3b83bb4aa482d95b1
Urva Gandhi <161921384+urvagandhi@users.noreply.github.com>
2026-02-11 18:11:26 +0530

    0890-LEMONADE-CHANGE Stats: Time: 15 ms (5.05%), Space: 78.9 MB (5.03%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.java
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -1,25 +1,36 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        int five = 0, ten = 0;
-        // int twenty = 0; (not needed as i'm not gonna give it to anyone)
 
-        for(int i  = 0; i < bills.length; i++){
-            if(bills[i] == 5) five++;
-            else if(bills[i] == 10 && five >= 1){
-                five--;
-                ten++;
-            }else if(bills[i] == 20){
-                if(five >= 1 && ten >=1){
-                    five--;
-                    ten--;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(5, 0);
+        map.put(10, 0);
+        map.put(20, 0);
+
+        for(int bill : bills){
+
+            if(bill == 5){
+                map.put(5, map.get(5) + 1);
+            }
+
+            else if(bill == 10){
+                if(map.get(5) == 0) return false;
+
+                map.put(5, map.get(5) - 1);
+                map.put(10, map.get(10) + 1);
+            }
+
+            else { // 20
+                if(map.get(10) > 0 && map.get(5) > 0){
+                    map.put(10, map.get(10) - 1);
+                    map.put(5, map.get(5) - 1);
                 }
-                else if(five >= 3){
-                    five = five - 3;
+                else if(map.get(5) >= 3){
+                    map.put(5, map.get(5) - 3);
                 }
                 else return false;
             }
-            else return false;
         }
+
         return true;
     }
 }

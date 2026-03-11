e426b471069f70f4ff8c64f078b5cb3cf5686b8b
Urva Gandhi <161921384+urvagandhi@users.noreply.github.com>
2026-03-11 19:39:48 +0530

    Time: 0 ms (100%), Space: 42.3 MB (50.09%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -1,12 +1,12 @@
 class Solution {
     public int bitwiseComplement(int n) {
-        // if (n == 0) return 1;
-        // int mask = (1 << (32 - Integer.numberOfLeadingZeros(n))) - 1;
-        // return n ^ mask;
+        if (n == 0) return 1;
+        int mask = (1 << (32 - Integer.numberOfLeadingZeros(n))) - 1;
+        return n ^ mask;
 
-        int mask = n | 1; // edge case n == 0
-        for (int i = 0; i <= 4; i++)
-            mask |= mask >> (1 << i);
-        return n ^ mask; // alternative
+        // int mask = n | 1; // edge case n == 0
+        // for (int i = 0; i <= 4; i++)
+        //     mask |= mask >> (1 << i);
+        // return n ^ mask; // alternative
     }
 }

5edb936553d75074c01dbe356704148ffbb5d1c3
Urva Gandhi <161921384+urvagandhi@users.noreply.github.com>
2026-06-06 20:39:40 +0530

    06-06-2026 0454-4sum-ii 107 ms (94.07%) 47.1 MB (15.28%) Medium .java Array

---
+++ LeetCode/Medium/0454-4sum-ii/0454-4sum-ii-06-06-2026-20-39-40.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int a : nums1) {
+            for(int b : nums2) {
+                map.put(a + b,
+                        map.getOrDefault(a + b, 0) + 1);
+            }
+        }
+
+        int count = 0;
+
+        for(int c : nums3) {
+            for(int d : nums4) {
+
+                int need = -(c + d);
+
+                count += map.getOrDefault(need, 0);
+            }
+        }
+
+        return count;
+    }
+}
